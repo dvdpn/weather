fd30cc29005e5f03c6f564ce9c08f34f54029296
dvdpn <davidpan2004@gmail.com>
2021-01-01 20:27:30 -0800

    Update stay_standalone.js

--- stay_standalone.js
+++ stay_standalone.js
@@ -1,23 +1,57 @@
-// Mobile Safari in standalone mode
-if(("standalone" in window.navigator) && window.navigator.standalone){
+static getIOSScope(): string {
+    const standalone = (window.navigator as any).standalone;
+    const userAgent = window.navigator.userAgent.toLowerCase();
+    const safari = /safari/.test(userAgent);
+    const ios = /iphone|ipod|ipad/.test(userAgent);
+    if (ios) {
+        if (!standalone && safari) {
+            return 'browser';
+        } else if (standalone && !safari) {
+            return 'standalone';
+        } else if (!standalone && !safari) {
+            return 'uiwebview';
+        }
+    }
+    return 'another';
+}
 
-	// If you want to prevent remote links in standalone web apps opening Mobile Safari, change 'remotes' to true
-	var noddy, remotes = false;
-	
-	document.addEventListener('click', function(event) {
-		
-		noddy = event.target;
-		
-		// Bubble up until we hit link or top HTML element. Warning: BODY element is not compulsory so better to stop on HTML
-		while(noddy.nodeName !== "A" && noddy.nodeName !== "HTML") {
-	        noddy = noddy.parentNode;
-	    }
-		
-		if('href' in noddy && noddy.href.indexOf('http') !== -1 && (noddy.href.indexOf(document.location.host) !== -1 || remotes))
-		{
-			event.preventDefault();
-			document.location.href = noddy.href;
-		}
-	
-	},false);
+static fixIOSLinks() {
+    const scope = Utils.getIOSScope();
+    if (scope !== 'standalone' && scope !== 'uiwebview') {
+        return;
+    }
+    // _blank links open inside
+    const links = document.getElementsByTagName('a');
+    for (let i = 0; i < links.length; ++i) {
+        if (links[i].getAttribute('target') == '_blank') {
+            links[i].removeAttribute('target');
+            links[i].setAttribute('data-target', '_blank');
+        }
+    }
+    // not _blank links open outside => iOS is piece of shit
+    document.addEventListener('click', function (e) {
+        let element: Element = e.target as Element;
+        while (!/^(a|html)$/i.test(element.nodeName) && element.parentNode) {
+            element = element.parentNode as Element;
+        }
+        if (element.getAttribute) {
+            const href = element.getAttribute('href');
+            const target = element.getAttribute('data-target');
+            if (target) {
+                return;
+            }
+            const protocol = (element as any).protocol;
+            const isHrefDefined = !!href && '#' !== href;
+            if (isHrefDefined) {
+                const isWebProtocol = !protocol || protocol !== 'tel:';
+                const isRelativePath = Utils.isRelativePath(href);
+                const pattern = new RegExp("^[a-z][a-z0-9+.-]*:\/\/" + window.document.location.host);
+                const isCurrentHost = pattern.test(href);
+                if (isWebProtocol && (isRelativePath || isCurrentHost)) {
+                    e.preventDefault();
+                    window.document.location.assign(href);
+                }
+            }
+        }
+    }, false);
 }
